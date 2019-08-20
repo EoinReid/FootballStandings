9d82ba8858294620f201ab56e06733b0094e42f3
EoinReid <40269943+EoinReid@users.noreply.github.com>
2019-08-20 21:11:12 +0100

    Adds View

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Football League and Standings Manager
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Football League Standings</title>
+        <link href="../main.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+
+    </head>
+
+    <body>
+        <header><h1>Football League Standings</h1>
+            <div class="topnav">
+                <a href="fixtures_manager/index.php">Fixtures Manager</a>
+                <a href="team_info/index.php">Team Information</a>
+        </header>
+
